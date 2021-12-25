1bd8be2ed92971cc3a54bdacf71af847e2a4c44a
Belal-hossainn <belalhossain.rn@gmail.com>
2021-12-26 00:46:04 +0600

    google sign in added

---
+++ src/components/Login/Config.firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBWcYj2rjroKL29oG2mrRoX0LlKmBjDnc",
+    authDomain: "burj-al-arab-7e7cd.firebaseapp.com",
+    projectId: "burj-al-arab-7e7cd",
+    storageBucket: "burj-al-arab-7e7cd.appspot.com",
+    messagingSenderId: "730372806137",
+    appId: "1:730372806137:web:3f5b17e180239812f89928"
+  };
+
+  export default firebaseConfig;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,37 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React, { useContext } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { UserContext } from '../../App';
+import firebaseConfig from './Config.firebase';
+const app = initializeApp(firebaseConfig);
 
 
 const Login = () => {
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
+  const provider = new GoogleAuthProvider();
+  const googleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const { displayName, email } = result.user;
+        const signedInUser = { name: displayName, email };
+        setLoggedInUser(signedInUser);
+        history.replace(from)
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorMessage = error.message;
+
+        // ...
+      });
+  };
     return (
         <div>
-            <h1>This is Login</h1>
+            <button onClick={googleSignIn}>Sign in with Google</button>
         </div>
     );
 };
